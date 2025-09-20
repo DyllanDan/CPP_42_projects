14289032a6dfba56a9a456853a1b6cec640a60d2
Dyllan Daniel De Paula De Oliveira <dydaniel@c3r2p2.42sp.org.br>
2025-09-20 17:02:15 -0300

    claptrap

--- CPP_03/ex00/ClapTrap.cpp
+++ CPP_03/ex00/ClapTrap.cpp
@@ -6,9 +6,78 @@
 /*   By: dydaniel <dydaniel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/13 18:15:52 by dydaniel          #+#    #+#             */
-/*   Updated: 2025/09/13 18:16:03 by dydaniel         ###   ########.fr       */
+/*   Updated: 2025/09/20 14:00:13 by dydaniel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ClapTrap.hpp"
 
+ClapTrap::ClapTrap()
+{
+    name = "";
+	hitPoints = 10;
+    energyPoints = 10;
+    atkDamage = 0;
+}
+
+ClapTrap::ClapTrap(std::string name)
+{
+    this->name = name;
+	hitPoints = 10;
+    energyPoints = 10;
+    atkDamage = 0;
+}
+
+ClapTrap::~ClapTrap(){}
+
+ClapTrap::ClapTrap(const ClapTrap& other)
+{
+	name = other.name;
+	hitPoints = other.hitPoints;
+	energyPoints = other.energyPoints;
+	atkDamage = other.atkDamage;
+}
+ClapTrap& ClapTrap::operator=(const ClapTrap& fixed)
+{
+	if (this != &fixed)
+	{
+		this->name = fixed.name;
+		this->hitPoints = fixed.hitPoints;
+		this->energyPoints = fixed.energyPoints;
+		this->atkDamage = fixed.atkDamage;
+	}
+	return (*this);
+}
+
+void ClapTrap::attack(const std::string& target)
+{
+	if (energyPoints > 0)
+	std::cout << "ClapTrap " << name << " attacks " << " target " << target;
+	std::cout << " causing " << atkDamage << " points of damage!" << std::endl;
+}
+void ClapTrap::takeDamage(unsigned int amount)
+{
+	if (hitPoints > 0)
+		this->hitPoints -= amount;
+	if (hitPoints < 0)
+		this->hitPoints = 0;
+	std::cout << "ClapTrap " << name << " receives " << amount << "of damage. Remaining health: ";
+	std::cout << hitPoints << std::endl;	
+}
+void ClapTrap::beRepaired(unsigned int amount)
+{
+	if (hitPoints > 0 && energyPoints > 0)
+		this->hitPoints += amount;
+	else if (hitPoints == 0)
+	{
+		std::cout << "ClapTrap " << name << " is defeated and cannot be repaired..." << std::endl;
+		return ;
+	}
+	else if (energyPoints == 0)
+	{
+		std::cout << "ClapTrap " << name << " has no energy left to repair itself." << std::endl;
+		return ;
+	}
+	std::cout << "ClapTrap " << name << " receives " << amount << "of damage. Remaining health: ";
+	std::cout << hitPoints << std::endl;	
+}
--- CPP_03/ex00/ClapTrap.hpp
+++ CPP_03/ex00/ClapTrap.hpp
@@ -6,7 +6,7 @@
 /*   By: dydaniel <dydaniel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/13 18:11:16 by dydaniel          #+#    #+#             */
-/*   Updated: 2025/09/13 18:18:44 by dydaniel         ###   ########.fr       */
+/*   Updated: 2025/09/20 13:47:35 by dydaniel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,14 +25,16 @@ class ClapTrap
     
     public:
     //Canonical
-        ClapTrap();
-        ~ClapTrap();
-        ClapTrap( const ClapTrap& other );
-		ClapTrap& operator=(const ClapTrap& fixed);
+        ClapTrap(); // Constructor
+        ~ClapTrap(); // Destructor
+        ClapTrap(const ClapTrap& other); //Copy
+		ClapTrap& operator=(const ClapTrap& fixed); //Overcharge =
+        ClapTrap(std::string name); // Constructor with name
     //methods
         void attack(const std::string& target);
         void takeDamage(unsigned int amount);
         void beRepaired(unsigned int amount);
+        std::string getName() const;
 };
 
 #endif
