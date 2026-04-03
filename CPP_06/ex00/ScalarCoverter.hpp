5257bca9a77903432d884fc820816dfc0806905e
Dyllan Daniel De Paula De Oliveira <dydaniel@c1r9p2.42sp.org.br>
2026-04-03 18:05:24 -0300

    a

---
+++ CPP_06/ex00/ScalarCoverter.hpp
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScalarCoverter.hpp                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dydaniel <dydaniel@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/03 18:03:19 by dydaniel          #+#    #+#             */
+/*   Updated: 2026/04/03 18:04:14 by dydaniel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SCALARCONVERTER_HPP
+#define SCALARCONVERTER_HPP
+
+class ScalarConverter
+{
+    
+}
+
+#endif
