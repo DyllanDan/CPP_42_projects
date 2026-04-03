e9a361a64d73fae549f970cfae91502a84f1b747
Dyllan Daniel De Paula De Oliveira <dydaniel@c1r10p2.42sp.org.br>
2026-04-03 18:37:21 -0300

    jesuita

--- CPP_06/ex00/ScalarConverter.cpp
+++ CPP_06/ex00/ScalarConverter.cpp
@@ -10,3 +10,61 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ScalarConverter.hpp"
+
+ScalarConverter::ScalarConverter(){}
+
+ScalarConverter::~ScalarConverter(){}
+
+ScalarConverter::ScalarConverter(const ScalarConverter& other)
+{
+    (void)other;
+}
+
+ScalarConverter& ScalarConverter::operator=(const ScalarConverter& other)
+{
+    (void)other;
+    return *this;
+}
+
+static void charConverter(std::string type)
+{
+    char c;
+    if (type.length() == 1)
+        c = type[0];
+    else if
+    {
+        int value = std::stoi(type)
+        if (value >= 32 && value <= 255)
+        {
+            c = static_cast<char>(value);
+        }
+        else
+        {
+            
+        }
+    }
+}
+
+static void intConverter(std::string type)
+{
+    
+}
+
+static void floatConverter(std::string type)
+{
+    
+}
+
+static void doubleConverter(std::string type)
+{
+    
+}
+
+void ScalarConverter::Converter(std::string type)
+{
+    charConverter(type);
+    intConverter(type);
+    floatConverter(type);
+    doubleConverter(type);
+}
--- CPP_06/ex00/ScalarCoverter.hpp
+++ CPP_06/ex00/ScalarCoverter.hpp
@@ -13,9 +13,18 @@
 #ifndef SCALARCONVERTER_HPP
 #define SCALARCONVERTER_HPP
 
+#include <iostream>
+
 class ScalarConverter
 {
-    
+    private:
+        ScalarConverter();
+        ~ScalarConverter();
+        ScalarConverter(const ScalarConverter& other);
+        ScalarConverter& operator=(const ScalarConverter& other);
+
+    public:
+        static void Converter(std::string type);
 }
 
 #endif
